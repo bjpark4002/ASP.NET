69e273f004a1a1ea16fccce5a60604e94ece25a1
Bumjin Park <bjpark4002@gmail.com>
2020-02-11 00:25:10 -0500

    -

---
+++ C#/delegatePractice/delegatePractice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace delegatePractice
+{
+    class Program
+    {
+        delegate void Test(int a, int b);
+
+        public static void Sum(int a, int b)
+        {
+            Console.WriteLine("{0} + {1} : {2}",a,b, a + b);
+        }
+        static public void Sub(int a, int b)
+        {
+            Console.WriteLine("{0} - {1} : {2}", a, b ,a - b);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Test tem = new Test(Sum);
+            tem(5, 10);
+            tem = Sub;
+            tem(10, 4);
+        }
+
+
+    }
+
+
+}
