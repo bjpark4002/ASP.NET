1e6f8dce1a56ab4ef7f1c33eecfca11e7565ace3
Bumjin Park <bjpark4002@gmail.com>
2020-02-18 00:13:22 -0500

    login

---
+++ TropicalServerApp/TropicalServerApp/TropicalServerApp/TropicalServerApp/Controllers/LoginController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TropicalServerApp.Models;
+using TropicalServerApp.Dal;
+
+namespace TropicalServerApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        // static user object 
+        // GET: Login
+        public ActionResult Login()
+        {
+            ViewData["MyTime"] = DateTime.Now.ToString();
+
+            Customer obj = new Customer();
+            return View("Login",obj);
+        }
+        
+        public ActionResult LoginSuccess()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ValidateLogin(Customer obj)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new CustomerDal())
+                {
+                    //Linq  
+                    //var user = db.Customers.FirstOrDefault(u=>u.LoginId == obj.LoginId && u.Password == obj.Password);
+
+                    var user = db.Customers.FirstOrDefault(u => u.LoginId.Equals(obj.LoginId) && u.Password.Equals(obj.Password)); 
+                    if(user != null)
+                    {
+                        //return View("AfterLogin", obj);
+                        return RedirectToAction("Orders", "Product");
+                    }
+                }
+                ModelState.AddModelError(string.Empty, "Incorrect ID/Password");
+                //return View("Login", obj);
+            }
+            return View("Login",obj);
+
+            //adding info into database.
+            //CustomerDal Dal = new CustomerDal(); 
+            //Dal.Customers.Add(obj);
+            //Dal.SaveChanges();
+            //if (ModelState.IsValid)
+            //{
+
+            //    return View("AfterLogin", obj);
+            //}
+            //else
+            //{
+            //    return View("Login", obj);
+            //}
+            //Customer obj = new Customer();
+
+            //obj.LoginId  = Request.Form["txtLoginId"];
+            //obj.Password = Request.Form["txtPassword"];
+        }
+    }
+}
---
+++ TropicalServerApp/TropicalServerApp/TropicalServerApp/TropicalServerApp/Dal/CustomerDal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TropicalServerApp.Models;
+namespace TropicalServerApp.Dal
+{
+    public class CustomerDal:DbContext
+    {
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Customer>().ToTable("tblLoginInfo");
+        }
+        public DbSet<Customer> Customers { get; set; }
+
+
+    }
+}
